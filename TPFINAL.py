488a20fa785f15b4ebec3a0325b9afbb8b327cff
Ludwig Saux <ludwig.saux@hotmail.com>
2023-12-08 15:59:25 +0100

    maj

--- TPFINAL.py
+++ TPFINAL.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import plotly.express as px
 import json
